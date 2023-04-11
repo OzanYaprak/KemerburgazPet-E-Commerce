a95960b90cca036f665570b51cf2773a33e848cf
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-04-11 03:55:14 +0300

    Product Create Page Created

--- KemerburgazPetShop.WebUI/Controllers/AdminController.cs
+++ KemerburgazPetShop.WebUI/Controllers/AdminController.cs
@@ -1,17 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KemerburgazPetShop.Business.Abstract;
+using KemerburgazPetShop.Entities;
+using KemerburgazPetShop.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KemerburgazPetShop.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
+        {
+            return View(new IndexViewModels()
+            {
+                
+            });
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
         {
             return View();
         }
 
-        public IActionResult CreateProduct()
+
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductViewModel model)
         {
-            return View();
+            var entity=new Product() 
+            {
+                ProductName = model.ProductName,
+                ImageURL = model.ImageURL,
+                ProductDescription = model.ProductDescription,
+                ProductPrice = model.ProductPrice
+            };
+            _productService.Create(entity);
+            return Redirect("Index");
         }
     }
 }
---
+++ KemerburgazPetShop.WebUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KemerburgazPetShop.WebUI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ImageURL { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
