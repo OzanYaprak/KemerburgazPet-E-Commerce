c0a0c40a4c01576d23c62d1548b9b036538d4c27
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-04-28 00:56:47 +0300

    Checkout page validations completed. minor design changes.

--- KemerburgazPetShop.WebUI/ViewModels/OrderViewModel.cs
+++ KemerburgazPetShop.WebUI/ViewModels/OrderViewModel.cs
@@ -1,20 +1,56 @@
-﻿namespace KemerburgazPetShop.WebUI.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit.Abstractions;
+
+namespace KemerburgazPetShop.WebUI.ViewModels
 {
     public class OrderViewModel
     {
         public CartModel CartModel { get; set; }
 
+        [Required(ErrorMessage = "Ad alanı gereklidir.")]
+        [StringLength(50, ErrorMessage = "Ad en fazla 50 karakter olmalıdır.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        [StringLength(50, ErrorMessage = "Soyad en fazla 50 karakter olmalıdır.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Adres alanı gereklidir.")]
+        [StringLength(100, ErrorMessage = "Adres en fazla 100 karakter olmalıdır.")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Şehir alanı gereklidir.")]
+        [StringLength(50, ErrorMessage = "Şehir en fazla 50 karakter olmalıdır.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Telefon alanı gereklidir.")]
+        [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "Geçersiz telefon numarası.")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "E-posta alanı gereklidir.")]
+        [EmailAddress(ErrorMessage = "Geçersiz e-posta adresi.")]
         public string Email { get; set; }
 
+
+
+        [Required(ErrorMessage = "Kart üzerinde bulunan isim alanı gereklidir.")]
+        [StringLength(50, ErrorMessage = "Kart üzerinde bulunan isim en fazla 50 karakter olmalıdır.")]
         public string CreditCardName { get; set; }
+
+        [Required(ErrorMessage = "Kart numarası alanı gereklidir.")]
+        [CreditCard(ErrorMessage = "Geçersiz kredi kartı numarası.")]
         public string CreditCardNumber { get; set; }
+
+        [Required(ErrorMessage = "Kartın son kullanma ayı alanı gereklidir.")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])$", ErrorMessage = "Geçersiz ay formatı.")]
         public string CreditCardExpMount { get; set; }
+
+        [Required(ErrorMessage = "Kartın son kullanma yılı alanı gereklidir.")]
+        [RegularExpression(@"^(20)\d{2}$", ErrorMessage = "Geçersiz yıl formatı.")]
         public string CreditCardExpYear { get; set; }
+
+        [Required(ErrorMessage = "CVV alanı gereklidir.")]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Geçersiz CVV formatı.")]
         public string CreditCardCvv { get; set; }
     }
 }
--- KemerburgazPetShop.WebUI/wwwroot/js/site.js
+++ KemerburgazPetShop.WebUI/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+//Checkout için yazılan scrpitler başlangıç
+var paymentMethod = document.getElementsByName("paymentmethod");
+var cardInfo = document.getElementById("cardInfo");
+var customText = document.getElementById("customText");
+
+for (var i = 0; i < paymentMethod.length; i++) {
+    paymentMethod[i].addEventListener("change", function () {
+        if (this.id === "credit") {
+            cardInfo.style.display = "";
+            customText.style.display = "none";
+        }
+        else if (this.id === "eft") {
+            cardInfo.style.display = "none";
+            customText.style.display = "block";
+            customText.innerText = "Hesap bilgilerimiz siparişiniz oluşturulduktan sonra tarafınıza mail olarak gönderilecektir.";
+        }
+    });
+}
+
+//Confirm delete için yazılan script, admin girişlerinde aktif.
+function confirmDelete() {
+    var result = confirm("Silmek istediğinize emin misiniz?");
+    if (result) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
